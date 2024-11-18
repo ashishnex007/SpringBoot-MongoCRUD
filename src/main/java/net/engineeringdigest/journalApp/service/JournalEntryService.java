e98692b6e14c9d21f0db2f49383c1fd60b9e4c52
AshishGowtham <122021623+AshishGowtham@users.noreply.github.com>
2024-11-18 18:39:58 +0530

    added @Transactional to maintain Atomicity

--- src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
@@ -7,6 +7,7 @@ import net.engineeringdigest.journalApp.repository.UserRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -21,12 +22,18 @@ public class JournalEntryService {
     @Autowired
     private UserService userService;
 
+    @Transactional // this annotation is used to ensure that the operation is atomic
     public void saveEntry(JournalEntry journalEntry, String userName) {
-        User user = userRepository.findByUserName(userName);
-        journalEntry.setDate(LocalDateTime.now());
-        JournalEntry saved = journalEntryRepository.save(journalEntry);
-        user.getJournalEntries().add(saved);
-        userService.saveEntry(user);
+        try {
+            User user = userRepository.findByUserName(userName);
+            journalEntry.setDate(LocalDateTime.now());
+            JournalEntry saved = journalEntryRepository.save(journalEntry);
+            user.getJournalEntries().add(saved);
+            userService.saveEntry(user);
+        }catch(Exception e){
+            System.out.print(e);
+            throw new RuntimeException("Error saving journal entry", e);
+        }
     }
 
     public void saveEntry(JournalEntry journalEntry) {
