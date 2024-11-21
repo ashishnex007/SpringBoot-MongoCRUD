1a1dd51ecf960c8134b088fbf6ae205f0b02eb10
AshishGowtham <122021623+AshishGowtham@users.noreply.github.com>
2024-11-21 13:30:24 +0530

    Added crons - schedulers with @Scheduling

---
+++ src/main/java/net/engineeringdigest/journalApp/scheduler/UserScheduler.java
@@ -0,0 +1,50 @@
+package net.engineeringdigest.journalApp.scheduler;
+
+import net.engineeringdigest.journalApp.cache.AppCache;
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+import net.engineeringdigest.journalApp.entity.User;
+import net.engineeringdigest.journalApp.repository.UserRepositoryImpl;
+import net.engineeringdigest.journalApp.service.EmailService;
+import net.engineeringdigest.journalApp.service.SentimentAnalysisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class UserScheduler {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Autowired
+    private UserRepositoryImpl userRepository;
+
+    @Autowired
+    private SentimentAnalysisService sentimentAnalysisService;
+
+    @Autowired
+    private AppCache appCache;
+
+    @Scheduled(cron = "0 0 9 * * SUN") // Run every Sunday at 9 AM
+    public void fetchUsersAndSendSAMail(){
+        List<User> users = userRepository.getUsersForSA();
+        for(User user: users){
+            List<JournalEntry> journalEntries = user.getJournalEntries();
+            List<String> filteredEntries = journalEntries.stream().filter(x -> x.getDate().isAfter(LocalDateTime.now().minus(7, ChronoUnit.DAYS))).map(x -> x.getContent()).collect(Collectors.toList());
+            String entry = String.join(" ", filteredEntries);
+            String sentiment = sentimentAnalysisService.getSentiment(entry);
+            emailService.sendEmail(user.getEmail(), "Sentiment Analysis Report", sentiment);
+        }
+    }
+
+    @Scheduled(cron = "0 */10 * * * ?") // Run every 10 minutes
+    public void clearAppCache(){
+        appCache.init();
+    }
+
+}
